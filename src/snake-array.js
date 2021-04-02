595dddfed181d4201cedf1ea1215511e2599ebe4
AndreyGorban <Whiteman1989@gmail.com>
2021-04-02 09:33:02 +0300

    Coments edited

--- src/snake-array.js
+++ src/snake-array.js
@@ -10,25 +10,25 @@ const snakeArray = (snakeStart) => {
     let verticalMax = rows-1;
 
     while (horizontalMin <= horizontalMax && verticalMin <= verticalMax) {
-        // Top row
+        // Horizontal forward
         for (let i = horizontalMin; i <= horizontalMax; i++) {
             matrix[verticalMin][i] = snakeStart;
           snakeStart++;
         }
         verticalMin++;
-    // Right column
+        // Down
         for (let i = verticalMin; i <= verticalMax; i++) {
             matrix[i][horizontalMax] = snakeStart;
           snakeStart++;
         }
         horizontalMax--;
-    // Bottom row
+        // Horizontal Backward
         for (let i = horizontalMax; i >= horizontalMin; i--) {
             matrix[verticalMax][i] = snakeStart;
           snakeStart++;
         }
         verticalMax--;
-    // start column
+        // Up
         for (let i = verticalMax; i >= verticalMin; i--) {
             matrix[i][horizontalMin] = snakeStart;
           snakeStart++;
